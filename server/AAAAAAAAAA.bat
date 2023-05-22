eea83eaa25b55c42efd7491718a3dde630a4c2d3
Your Name <you@example.com>
2023-05-23 00:07:56 +0800

    Create AAAAAAAAAA.bat

---
+++ server/AAAAAAAAAA.bat
@@ -0,0 +1,12 @@
+python MMVCServerSIO.py ^
+More?     -p 18888 ^
+More?     --https false ^
+More?     --content_vec_500 pretrain/checkpoint_best_legacy_500.pt ^
+More?     --content_vec_500_onnx pretrain/checkpoint_best_legacy_500.onnx ^
+More?     --content_vec_500_onnx_on false ^
+More?     --hubert_base pretrain/hubert_base.pt ^
+More?     --hubert_base_jp pretrain/rinna_hubert_base_jp.pt ^
+More?     --hubert_soft pretrain/hubert/hubert-soft-0d54a1f4.pt ^
+More?     --nsf_hifigan pretrain/nsf_hifigan/model ^
+More?     --model_dir models ^
+More?     --samples samples.json
