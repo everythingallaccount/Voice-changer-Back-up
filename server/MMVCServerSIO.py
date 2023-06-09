7a812331ffcb45c83ecb326144a1c9eaef3c2a62
Your Name <you@example.com>
2023-06-10 00:48:34 +0800

    Update MMVCServerSIO.py

--- server/MMVCServerSIO.py
+++ server/MMVCServerSIO.py
@@ -345,21 +345,26 @@ if __name__ == "__main__":
 
         p = mp.Process(name="p", target=localServer, args=(args.logLevel,))
         p.start()
-        try:
-            if sys.platform.startswith("win"):
-                process = subprocess.Popen(
-                    [NATIVE_CLIENT_FILE_WIN, "-u", f"http://localhost:{PORT}/"]
-                )
-                return_code = process.wait()
-                print("client closed.")
-                p.terminate()
-            elif sys.platform.startswith("darwin"):
-                process = subprocess.Popen(
-                    [NATIVE_CLIENT_FILE_MAC, "-u", f"http://localhost:{PORT}/"]
-                )
-                return_code = process.wait()
-                print("client closed.")
-                p.terminate()
 
-        except Exception as e:
-            print(e)
+        printMessage(f"P should be started.", level=1)
+
+        
+        if(0):
+            try:
+                if sys.platform.startswith("win"):
+                    process = subprocess.Popen(
+                        [NATIVE_CLIENT_FILE_WIN, "-u", f"http://localhost:{PORT}/"]
+                    )
+                    return_code = process.wait()
+                    print("client closed.")
+                    p.terminate()
+                elif sys.platform.startswith("darwin"):
+                    process = subprocess.Popen(
+                        [NATIVE_CLIENT_FILE_MAC, "-u", f"http://localhost:{PORT}/"]
+                    )
+                    return_code = process.wait()
+                    print("client closed.")
+                    p.terminate()
+
+            except Exception as e:
+                print(e)
